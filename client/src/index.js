51c68e61f283e2750fd35a5d5f1bb53109658eae
samidemiroluk@gmail.com <samidemiroluk@gmail.com>
2020-04-20 17:49:11 -0400

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+import Routes from './routes';
+
+const createStoreWithMiddleWare = applyMiddleware(promiseMiddleware,ReduxThunk)(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)}>
+    <BrowserRouter>
+      <Routes/>
+  </BrowserRouter>
+  </Provider>
+,document.getElementById('root'))
